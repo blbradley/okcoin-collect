ac35a4238350d1a023780f4d6977f6ea03778853
Brandon Bradley <bradleytastic@gmail.com>
2016-10-14 14:01:47 -0500

    refactor into main function (#3)

--- main.py
+++ main.py
@@ -17,17 +17,12 @@ max_retries = int(os.environ['MAX_RETRIES'])
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(levelname)s %(message)s')
 
-ws = create_connection('wss://real.okcoin.cn:10440/websocket/okcoinapi')
-
 subs = [
     {'event':'addChannel', 'channel':'ok_sub_spotcny_btc_ticker'},
     {'event':'addChannel', 'channel':'ok_sub_spotcny_btc_depth_60'},
     {'event':'addChannel', 'channel':'ok_sub_spotcny_btc_trades'},
 ]
-msg = json.dumps(subs)
-
-ws.send(msg)
-
+initial_msg = json.dumps(subs)
 
 key_schema = json.dumps({'type': 'string'})
 value_schema = json.dumps({
@@ -42,28 +37,35 @@ value_schema = json.dumps({
 
 def create_producer_request(key, value):
     headers = {'Content-Type':'application/vnd.kafka.avro.v1+json'}
-    data = {'key_schema': key_schema, 'value_schema': value_schema, 'records': [{'key': exchange, 'value': value}]}
+    data = {'key_schema': key_schema, 'value_schema': value_schema, 'records': [{'key': key, 'value': value}]}
     return Request('POST', kafka_rest_proxy_url + '/topics/exchanges_raw', data=json.dumps(data), headers=headers)
 
-s = Session()
-while True:
-    response = ws.recv()
+def main():
+    ws = create_connection('wss://real.okcoin.cn:10440/websocket/okcoinapi')
+    ws.send(initial_msg)
 
-    timestamp = datetime.utcnow()
-    exchange = 'okcoin'
-    value = {'time': timestamp.isoformat(), 'exchange': exchange, 'event': response}
-    req = create_producer_request(exchange, value)
+    s = Session()
+    while True:
+        response = ws.recv()
 
-    prepped = s.prepare_request(req)
-    retry_count = 0
-    while retry_count < max_retries:
-        resp = s.send(prepped)
-        if resp.status_code == 200:
-            break
-        logging.error('[%s] %d %s', resp.headers['Date'], resp.status_code, resp.text)
+        timestamp = datetime.utcnow()
+        exchange = 'okcoin'
+        value = {'time': timestamp.isoformat(), 'exchange': exchange, 'event': response}
+        req = create_producer_request(exchange, value)
 
-        # fixed retry delay
-        sleep(3)
-        retry_count += 1
-    if retry_count >= max_retries:
-        raise RuntimeError('Exceeded maximum number of retries.')
+        prepped = s.prepare_request(req)
+        retry_count = 0
+        while retry_count < max_retries:
+            resp = s.send(prepped)
+            if resp.status_code == 200:
+                break
+            logging.error('[%s] %d %s', resp.headers['Date'], resp.status_code, resp.text)
+
+            # fixed retry delay
+            sleep(3)
+            retry_count += 1
+        if retry_count >= max_retries:
+            raise RuntimeError('Exceeded maximum number of retries.')
+
+if __name__ == "__main__":
+    main()
